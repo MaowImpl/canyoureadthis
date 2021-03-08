f588009e0d30243101f57e555e5e2e52f3442569
Maow <28311382+MaowImpl@users.noreply.github.com>
2021-03-08 12:12:49 -0500

    Add files via upload

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+private data class Word(private val string: String) {
+    val start = string[0]
+    val middle = string.substring(1 .. string.length - 2)
+    val end = string[string.length - 1]
+}
+
+fun main(args: Array<String>) {
+    if (args.size == 1) {
+        val builder = StringBuilder()
+        args[0].split(' ').forEach {
+            val word = Word(it)
+            val string: String =
+                if (it.length > 3)
+                    word.start + word.middle.shuffle() + word.end
+                else it
+            builder
+                .append(string)
+                .append(' ')
+        }
+        println(builder.toString())
+    }
+}
+
+private fun String.shuffle(): String {
+    return this
+        .toList()
+        .shuffled()
+        .joinToString("")
+}
